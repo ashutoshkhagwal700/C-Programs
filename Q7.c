3ce8e2fd40e92b4754a82d4eb03389c2493bffa5
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-02-27 21:01:00 +0530

    Add files via upload

---
+++ Q7.c
@@ -0,0 +1,9 @@
+// program to calculate sum of two integers
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter two num");
+    scanf("%d %d",&a,&b);
+    printf("sum = %d",a+b);
+    return 0;
+}
