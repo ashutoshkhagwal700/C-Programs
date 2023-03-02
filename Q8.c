c337e2b5646dd06e0687c0f7c4375e88160cacc8
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-03-02 18:02:54 +0530

    Add files via upload

---
+++ Q8.c
@@ -0,0 +1,10 @@
+// program to calculate square of given numbers
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter number");
+    scanf("%d",&n);
+    printf("square %d",n*n);
+    return 0;
+    
+}
