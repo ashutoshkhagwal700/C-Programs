56fc9d034ecf8f17fc484b6f30b4daec40107766
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-03-07 21:01:01 +0530

    Add files via upload
    code for calculate area of rectangle

---
+++ Q9.c
@@ -0,0 +1,10 @@
+// program to calculate area of rectangle
+#include<stdio.h>
+int main(){
+    int l,b,area;
+    printf("enter l and b");
+    scanf("%d%d",&l,&b);
+    area=l*b;
+    printf("area is %d",area);
+    return 0;
+}
