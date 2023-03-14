6bb87bc15972c9adbd343860d071f5e982abadc9
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-03-14 22:38:43 +0530

    code to find circumference of circle

---
+++ Q12.c
@@ -0,0 +1,11 @@
+// program to find circumference of circle
+#include<stdio.h>
+int main(){
+    float cir,pi=3.14,r;
+    printf("enter radius");
+    scanf("%f",&r);
+    cir=2*pi*r;
+    printf("circumference is %f",cir);
+    return 0;
+    
+}
