42559c4e403cf8ea6d9175e9689b5f8e285f6832
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-06-19 21:43:20 +0530

    Add files via upload

---
+++ Q16.c
@@ -0,0 +1,10 @@
+// Program to input a character from user and print its ASCII code.
+
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("enter char= ");
+    scanf("%c",&ch);
+    printf("ASCII value = %d",ch);
+    return 0;
+}
