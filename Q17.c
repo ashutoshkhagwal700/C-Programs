c8fb1657e8a67ee6918a211aa9a4912545a1a51b
AshutoshKhagwal <ashutoshkhagwal700@gmail.com>
2023-06-20 20:13:17 +0530

    ASCII Code

---
+++ Q17.c
@@ -0,0 +1,10 @@
+// Program to input ASCII code from the user and print its corresponding character.
+
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter ASCII code=");
+    scanf("%d",&n);
+    printf("character value=%c",n);
+    return 0;
+}
