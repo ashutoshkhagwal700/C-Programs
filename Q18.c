db158ca59d23f90d4724ffa4a1c72175e5680a6a
AshutoshKhagwal <ashutoshkhagwal700@gmail.com>
2023-06-21 21:10:43 +0530

    ASCII-code

---
+++ Q18.c
@@ -0,0 +1,13 @@
+// Program to input three character from user and display character with their corresponding ASCII codes.
+
+#include<stdio.h>
+int main(){
+    char a,b,c;
+    printf("enter character");
+    scanf("%c%c%c",&a,&b,&c);
+    printf("%c - %d\n",a,a);
+    printf("%c - %d\n",b,b);
+    printf("%c - %d\n",c,c);
+    return 0;
+
+}
