544bd4efb469f06e07922877c9150a1a97457665
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-07-05 16:02:47 +0530

    commit

---
+++ Q33.c
@@ -0,0 +1,15 @@
+// Program to print size of an int, a float, a char and a double type variable.
+
+#include<stdio.h>
+int main(){
+    int i;
+    float f;
+    char c;
+    double d;
+    printf("size of i= %d\n",sizeof(i));
+    printf("size of f= %d\n",sizeof(f));
+    printf("size of c= %d\n",sizeof(c));
+    printf("size of d= %d\n",sizeof(d));
+    return 0;
+
+}
