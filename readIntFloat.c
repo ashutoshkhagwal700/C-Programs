e1a750824383294360baf481caa40c2146306040
Ashutosh Khagwal <ashutoshkhagwal700@gmail.com>
2023-07-24 17:34:00 +0530

    commit

---
+++ readIntFloat.c
@@ -0,0 +1,15 @@
+// Program to read two integer and two floating point numbers.
+
+#include<stdio.h>
+int main() {
+
+    int n;
+    float f;
+    printf("enter integer value");
+    scanf("%d",&n);
+    printf("enter floating value");
+    scanf("%f",&f);
+    printf("integer=%d\n",n);
+    printf("float=%f",f);
+    return 0;
+}
